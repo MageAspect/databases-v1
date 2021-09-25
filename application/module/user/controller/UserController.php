323320a0c8927db231649a6ba93dc5f8edf8ddeb
mosowell <mark.proxorofff@gmail.com>
2021-09-25 21:41:46 +0300

    Обработка ошибок в списке пользователей

--- application/module/user/controller/UserController.php
+++ application/module/user/controller/UserController.php
@@ -4,20 +4,28 @@ namespace application\module\user\controller;
 
 
 use application\core\AuthorizedController;
+use application\module\user\facade\UserFacadeException;
 
 
 class UserController extends AuthorizedController {
     public function listAction () {
         $page = $this->getDefaultPage();
         $page->contentFile = realpath(__DIR__ . '/../pages/list.php');
-
         $page->title = 'Список пользователей';
 
+        try {
+            $users = $this->userFacade->getAllUsers();
+            $page->data['current-user'] = $this->userFacade->getCurrentUser();
+            $page->data['users'] = $users;
 
-        $users = $this->userFacade->getAllUsers();
-        $page->data['current-user'] = $this->userFacade->getCurrentUser();
-        $page->data['users'] = $users;
+        } catch (UserFacadeException $e) {
+            $page->data['errors'][] = $e->getMessage();
+        }
 
         $this->view->render($page);
+    }
+
+    public function detailsUser(array $pageParams) {
+
     }
 }
--- application/module/user/pages/list.php
+++ application/module/user/pages/list.php
@@ -8,6 +8,7 @@ use application\module\user\entity\User;
 
 
 $allUsers = $PAGE_DATA['users'];
+$errors = $PAGE_DATA['errors'];
 $currentUser = $PAGE_DATA['current-user'];
 ?>
 
@@ -27,46 +28,50 @@ $currentUser = $PAGE_DATA['current-user'];
             <?php endif; ?>
         </div>
         <div class="main-grid-table users-list">
-            <div class="main-grid-table-row main-grid-table-row-head">
-                <div class="main-grid-table-row-column"></div>
-                <div class="main-grid-table-row-column">id</div>
-                <div class="main-grid-table-row-column">Фамилия</div>
-                <div class="main-grid-table-row-column">Имя</div>
-                <div class="main-grid-table-row-column">Отчество</div>
-                <div class="main-grid-table-row-column">email</div>
-                <div class="main-grid-table-row-column">Должность</div>
-                <div class="main-grid-table-row-column">Телефон</div>
-            </div>
-            <?php foreach ($allUsers as $user): ?>
-                <div class="main-grid-table-row">
-                    <div class="main-grid-table-row-column main-grid-table-row-actions">
-                        <i class="fas fa-bars"></i>
-                        <div class="main-grid-table-row-actions-container" style="display: none">
+            <?php if (empty($errors)): ?>
+                <div class="main-grid-table-row main-grid-table-row-head">
+                    <div class="main-grid-table-row-column"></div>
+                    <div class="main-grid-table-row-column">id</div>
+                    <div class="main-grid-table-row-column">Фамилия</div>
+                    <div class="main-grid-table-row-column">Имя</div>
+                    <div class="main-grid-table-row-column">Отчество</div>
+                    <div class="main-grid-table-row-column">email</div>
+                    <div class="main-grid-table-row-column">Должность</div>
+                    <div class="main-grid-table-row-column">Телефон</div>
+                </div>
+                <?php foreach ($allUsers as $user): ?>
+                    <div class="main-grid-table-row">
+                        <div class="main-grid-table-row-column main-grid-table-row-actions">
+                            <i class="fas fa-bars"></i>
+                            <div class="main-grid-table-row-actions-container" style="display: none">
                                 <div class="main-grid-table-row-actions-item main-grid-table-row-actions-item-view"
                                      onclick="location = '/users/<?= $user->id ?>/details'">
                                     Посмотреть
                                 </div>
-                            <?php if ($currentUser->isAdmin): ?>
-                                <div class="main-grid-table-row-actions-item main-grid-table-row-actions-item-edit"
-                                     onclick="location = '/users/<?= $user->id ?>/edit'">
-                                    Изменить
-                                </div>
-                                <div class="main-grid-table-row-actions-item main-grid-table-row-actions-item-delete"
-                                     onclick="location = '/users/<?= $user->id ?>/delete'">
-                                    Удалить
-                                </div>
-                            <?php endif; ?>
+                                <?php if ($currentUser->isAdmin): ?>
+                                    <div class="main-grid-table-row-actions-item main-grid-table-row-actions-item-edit"
+                                         onclick="location = '/users/<?= $user->id ?>/edit'">
+                                        Изменить
+                                    </div>
+                                    <div class="main-grid-table-row-actions-item main-grid-table-row-actions-item-delete"
+                                         onclick="location = '/users/<?= $user->id ?>/delete'">
+                                        Удалить
+                                    </div>
+                                <?php endif; ?>
+                            </div>
                         </div>
+                        <div class="main-grid-table-row-column"><?= $user->id ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->lastName ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->name ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->patronymic ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->email ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->position ?></div>
+                        <div class="main-grid-table-row-column"><?= $user->phone ?></div>
                     </div>
-                    <div class="main-grid-table-row-column"><?= $user->id ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->lastName ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->name ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->patronymic ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->email ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->position ?></div>
-                    <div class="main-grid-table-row-column"><?= $user->phone ?></div>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div class="application-content-error"><?= array_shift($errors) ?></div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
