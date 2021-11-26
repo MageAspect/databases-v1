c8fb68afe08696d95d34a373582314971fca3cd6
mosowell <mark.proxorofff@gmail.com>
2021-11-26 10:12:16 +0300

    Чтение конфига подключения к базе данных через переменные окружения

---
+++ application/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+        'host' => $_ENV['DB_HOST'],
+        'dbname' => $_ENV['DB_NAME'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'options' => array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        )
+);
--- application/core/db/DbConnection.php
+++ application/core/db/DbConnection.php
@@ -15,7 +15,7 @@ class DbConnection {
         if (empty(static::$dbConnection)) {
             $config = require $_SERVER['DOCUMENT_ROOT'] . '/application/config/db.php';
             static::$dbConnection = new PDO(
-                    'mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'] . ';charset=' . $config['charset'],
+                    'pgsql:host=' . $config['host'] . ';dbname=' . $config['dbname'] . ';port=5432',
                     $config['user'],
                     $config['password'],
                     $config['options']
